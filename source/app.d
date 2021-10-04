6a07958ea1e6ebd4e66decc23de3b4e7b98a52a5
Kostiantyn Tokar <tokar.kostya@gmail.com>
2021-10-04 14:30:13 +0300

    Open window

--- source/app.d
+++ source/app.d
@@ -1,5 +1,33 @@
 import std.stdio;
+import erupted.vulkan_lib_loader;
+import erupted;
+import bindbc.glfw;
 
 void main()
 {
+    "Hello, World!".writeln;
+
+    if(!loadGlobalLevelFunctions())
+    {
+        "Oops".writeln;
+        return;
+    }
+
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnVulkan", null, null);
+
+    uint extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(null, &extensionCount, null);
+
+    writeln(extensionCount, " extensions supported");
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
