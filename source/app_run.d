1349845b467c64292b9d23876aa04bedd3461175
Kostiantyn Tokar <tokar.kostya@gmail.com>
2021-10-06 19:18:06 +0300

    Refactoring

---
+++ source/app_run.d
@@ -0,0 +1,198 @@
+module app_run;
+
+import from : from;
+
+auto ref run() nothrow @nogc @safe
+{
+    import std.typecons : tuple;
+    import expected : andThen;
+    return initWindow(tuple())
+        .andThen!initVulkan
+        .andThen!mainLoop
+        .andThen!cleanup;
+}
+
+private:
+
+enum WindowWidth = 800;
+enum WindowHeight = 600;
+
+auto ref initWindow(T)(auto ref T arg) nothrow @nogc @trusted
+    if(from!"std.typecons".isTuple!T)
+{
+    import util : TupleCat;
+    import core.lifetime : forward, move;
+    import std.typecons : Tuple;
+    import glfw_vulkan : glfwInit, glfwWindowHint, glfwCreateWindow, GLFWwindow,
+            GLFW_CLIENT_API, GLFW_NO_API, GLFW_RESIZABLE, GLFW_FALSE;
+    import expected : ok;
+
+    alias Res = TupleCat!(T, Tuple!(GLFWwindow*, "window"));
+
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    auto window = glfwCreateWindow(WindowWidth, WindowHeight, "LearnVulkan", null, null);
+    return ok(Res(forward!arg.expand, window.move));
+}
+
+auto ref initVulkan(T)(auto ref T arg) nothrow @nogc @trusted
+    if(from!"std.typecons".isTuple!T)
+{
+    import core.lifetime : forward;
+    import std.meta : AliasSeq;
+    import erupted.vulkan_lib_loader : loadGlobalLevelFunctions;
+    import erupted : loadInstanceLevelFunctions;
+    import expected : ok, err, andThen;
+
+    return (loadGlobalLevelFunctions() ? ok(forward!arg) : err!T("Failed to load Vulkan global level functions."))
+        .andThen!createInstance
+        .andThen!((auto ref t) @trusted { loadInstanceLevelFunctions(t.instance); return ok(forward!t); });
+}
+
+auto ref createInstance(T)(auto ref T arg) nothrow @nogc @trusted
+    if(from!"std.typecons".isTuple!T)
+{
+    import util : TupleCat;
+    import core.lifetime : forward, move;
+    import std.typecons : Tuple;
+    import std.meta : AliasSeq;
+    import erupted : VkApplicationInfo, VkInstanceCreateInfo, vkCreateInstance, VkInstance,
+        VK_MAKE_API_VERSION, VK_API_VERSION_1_0, VK_SUCCESS;
+    import glfw_vulkan : glfwGetRequiredInstanceExtensions;
+    import expected : ok, err;
+    
+    alias Res = TupleCat!(T, Tuple!(VkInstance, "instance"));
+
+    debug(LearnVulkan_ValidationLayers)
+    {
+        if(!checkValidationLayerSupport())
+        {
+            return err!Res("Requested validation layers are not available.");
+        }
+    }
+
+    VkApplicationInfo appInfo;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+    appInfo.pEngineName = "No engine";
+    appInfo.engineVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint glfwExtensionCount;
+    const(char)** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    createInfo.enabledLayerCount = 0;
+
+    version(LearnVulkan_PrintExtensions)
+    () @trusted {
+        import erupted : vkEnumerateInstanceExtensionProperties, VkExtensionProperties;
+        import std.experimental.allocator.mallocator : Mallocator;
+        import std.experimental.allocator : makeArray, dispose;
+        import std.stdio : printf;
+        uint extensionCount;
+        vkEnumerateInstanceExtensionProperties(null, &extensionCount, null);
+        auto extensions = Mallocator.instance.makeArray!VkExtensionProperties(extensionCount);
+        scope(exit) () @trusted { Mallocator.instance.dispose(extensions); }();
+        vkEnumerateInstanceExtensionProperties(null, &extensionCount, extensions.ptr);
+        printf("Available extensions:\n");
+        foreach(ref const extension; extensions)
+        {
+            printf("\t");
+            printf(extension.extensionName.ptr);
+            printf("\n");
+        }
+    }();
+
+    VkInstance instance;
+
+    return vkCreateInstance(&createInfo, null, &instance) == VK_SUCCESS
+        ? ok(Res(forward!arg.expand, instance.move))
+        : err!Res("Failed to create VkInstance.");
+}
+
+debug(LearnVulkan_ValidationLayers)
+{
+    bool checkValidationLayerSupport() nothrow @nogc @trusted
+    {
+        import util : strcmp;
+        import core.stdc.string : strncmp;
+        import erupted : vkEnumerateInstanceLayerProperties, VkLayerProperties;
+        import std.experimental.allocator.mallocator : Mallocator;
+        import std.experimental.allocator : makeArray, dispose;
+
+        uint layerCount;
+        vkEnumerateInstanceLayerProperties(&layerCount, null);
+        auto availableLayers = Mallocator.instance.makeArray!VkLayerProperties(layerCount);
+        scope(exit) () @trusted { Mallocator.instance.dispose(availableLayers); }();
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.ptr);
+
+        static immutable validationLayers = ["VK_LAYER_KHRONOS_validation"];
+
+        foreach (ref const layerName; validationLayers)
+        {
+            auto layerFound = false;
+
+            foreach (ref const layerProperties; availableLayers)
+            {
+                if(strcmp(layerName, layerProperties.layerName.ptr) == 0)
+                {
+                    layerFound = true;
+                    break;
+                }
+            }
+
+            if(!layerFound)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+auto ref mainLoop(T)(auto ref T arg) nothrow @nogc @trusted
+    if(from!"std.typecons".isTuple!T
+        && is(typeof(arg.window) : from!"bindbc.glfw".GLFWwindow*))
+{
+    import core.lifetime : forward;
+    import glfw_vulkan : glfwWindowShouldClose, glfwPollEvents;
+    import expected : ok;
+
+    while(!glfwWindowShouldClose(arg.window))
+    {
+        glfwPollEvents();
+    }
+
+    return ok(forward!arg);
+}
+
+auto ref cleanup(T)(auto ref T arg) nothrow @nogc @trusted
+    if(from!"std.typecons".isTuple!T
+        && is(typeof(arg.window) : from!"bindbc.glfw".GLFWwindow*)
+        && is(typeof(arg.instance) : from!"erupted".VkInstance))
+{
+    import util : erase;
+    import core.lifetime : forward;
+    import erupted : vkDestroyInstance;
+    import erupted.vulkan_lib_loader : freeVulkanLib;
+    import glfw_vulkan : glfwDestroyWindow, glfwTerminate;
+    import expected : ok;
+
+    vkDestroyInstance(arg.instance, null);
+
+    glfwDestroyWindow(arg.window);
+    glfwTerminate();
+
+    freeVulkanLib();
+
+    return ok(forward!arg.erase!("window", "instance"));
+}
--- source/util.d
+++ source/util.d
@@ -1,5 +1,81 @@
 module util;
 
+import from : from;
+
+template TupleCat(Ts...)
+    if(from!"std.meta".allSatisfy!(from!"std.typecons".isTuple, Ts))
+{
+    import std.meta : AliasSeq;
+    import std.typecons : Tuple;
+    alias A = AliasSeq!();
+    static foreach(T; Ts)
+    {
+        static foreach(i; 0 .. T.Types.length)
+        {
+            static if(T.fieldNames[i] == "")
+            {
+                A = AliasSeq!(A, T.Types[i]);
+            }
+            else
+            {
+        		A = AliasSeq!(A, T.Types[i], T.fieldNames[i]);
+            }
+        }
+    }
+    alias TupleCat = Tuple!A;
+}
+
+template TupleErase(T, memberNames...)
+{
+    enum remaining = calcRemaining!(T, memberNames)();
+    alias TupleErase = TupleEraseImpl!(T, remaining);
+}
+
+template erase(memberNames...)
+    if(from!"std.meta".allSatisfy!(from!"std.traits".isSomeString, typeof(memberNames)))
+{
+    import core.lifetime : forward;
+
+    auto ref erase(T)(auto ref T t)
+        if(from!"std.typecons".isTuple!T)
+    {
+        static auto genCode(in bool[T.Types.length] remaining)
+        {
+            import std.conv : to;
+            string[] args;
+            foreach(i, remains; remaining)
+            {
+                if(remains) args ~= ["forward!t[" ~ i.to!string ~ "]"];
+            }
+            auto argsStr = "";
+            if(args.length != 0)
+            {
+                argsStr ~= args[0];
+                args = args[1 .. $];
+            }
+            foreach(arg; args)
+                argsStr ~= ", " ~ arg;
+            return q{return TupleEraseImpl!(T, remaining)(} ~ argsStr ~ ");";
+        }
+        enum remaining = calcRemaining!(T, memberNames)();
+        mixin(genCode(remaining));
+    }
+}
+
+void println(in string str) nothrow @nogc @safe
+{
+    import std.experimental.allocator : makeArray, dispose;
+    import std.experimental.allocator.mallocator : Mallocator;
+    import std.stdio : printf;
+
+    auto cStr = Mallocator.instance.makeArray!char(str.length + 2);
+    scope(exit) () @trusted { Mallocator.instance.dispose(cStr); }();
+    cStr[0 .. str.length] = str[];
+    cStr[str.length] = '\n';
+    cStr[str.length + 1] = '\0';
+    () @trusted { printf(cStr.ptr); }();
+}
+
 int strcmp(in string str, in char* cStr) pure nothrow @nogc @trusted
 {
     import core.stdc.string : strncmp;
@@ -7,4 +83,49 @@ int strcmp(in string str, in char* cStr) pure nothrow @nogc @trusted
     auto res = strncmp(&str[0], cStr, str.length);
     if(res != 0) return res;
     return cStr[str.length];
+}
+
+private template calcRemaining(T, memberNames...)
+    if(from!"std.typecons".isTuple!T)
+{
+    bool[T.Types.length] calcRemaining()
+    {
+        bool[T.Types.length] res;
+        static foreach(i; 0 .. T.Types.length)
+        {
+            res[i] = true;
+            foreach(name; memberNames)
+            {
+                if(T.fieldNames[i] == name)
+                {
+                    res[i] = false;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+private template TupleEraseImpl(T, alias remaining)
+{
+    import std.typecons : Tuple;
+    import std.meta : AliasSeq;
+
+    alias A = AliasSeq!();
+    static foreach(i; 0 .. T.Types.length)
+    {
+        static if(remaining[i])
+        {
+            static if(T.fieldNames[i] == "")
+            {
+                A = AliasSeq!(A, T.Types[i]);
+            }
+            else
+            {
+                A = AliasSeq!(A, T.Types[i], T.fieldNames[i]);
+            }
+        }
+    }
+    alias TupleEraseImpl = Tuple!A;
 }
