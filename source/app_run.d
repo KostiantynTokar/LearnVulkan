87660f73f6fd60b75697d8e1b4899cbe7de55ffb
Kostiantyn Tokar <k.tokar@samsung.com>
2021-11-01 12:31:33 +0200

    Transfer depth image to optimal layout

--- source/app_run.d
+++ source/app_run.d
@@ -1491,6 +1491,8 @@ auto ref createDepthResources(T)(auto ref T arg) nothrow @nogc @trusted
 if(from!"std.typecons".isTuple!T
     && is(typeof(arg.physicalDevice) : from!"erupted".VkPhysicalDevice)
     && is(typeof(arg.swapChainExtent) : from!"erupted".VkExtent2D)
+    && is(typeof(arg.commandPool) : from!"erupted".VkCommandPool)
+    && is(typeof(arg.graphicsQueue) : from!"erupted".VkQueue)
 )
 {
     import util;
@@ -1529,6 +1531,12 @@ if(from!"std.typecons".isTuple!T
                 res.depthImage = depthImage;
                 res.depthImageMemory = depthImageMemory;
                 res.depthImageView = depthImageView;
+                transitionImageLayout(
+                    res.device, res.commandPool, res.graphicsQueue,
+                    res.depthImage, depthFormat,
+                    VK_IMAGE_LAYOUT_UNDEFINED,
+                    VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL
+                );
                 return ok(res);
             })
             ;
@@ -1776,7 +1784,7 @@ void transitionImageLayout(
     from!"erupted".VkCommandPool commandPool,
     from!"erupted".VkQueue transferQueue,
     from!"erupted".VkImage image,
-    from!"erupted".VkFormat format, // TODO: superfluous?
+    from!"erupted".VkFormat format,
     from!"erupted".VkImageLayout oldLayout,
     from!"erupted".VkImageLayout newLayout,
 ) nothrow @nogc @trusted
@@ -1796,7 +1804,6 @@ void transitionImageLayout(
         // Specify concrete image part to be affected (mip level and array indices for arrays of images).
         subresourceRange :
         {
-            aspectMask : VK_IMAGE_ASPECT_COLOR_BIT,
             baseMipLevel : 0,
             levelCount : 1,
             baseArrayLayer : 0,
@@ -1809,6 +1816,7 @@ void transitionImageLayout(
 
     if(oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL)
     {
+        barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
         barrier.srcAccessMask = 0;
         barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
         sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
@@ -1816,10 +1824,25 @@ void transitionImageLayout(
     }
     else if(oldLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
     {
+        barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
         barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
         barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
         sourceStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
         destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+    }
+    else if(oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL)
+    {
+        barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
+        if(hasStencilComponent(format))
+        {
+            barrier.subresourceRange.aspectMask |= VK_IMAGE_ASPECT_STENCIL_BIT;
+        }
+        barrier.srcAccessMask = 0;
+        barrier.dstAccessMask =
+            VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT
+            | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+        sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+        destinationStage = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
     }
     else
     {
@@ -1893,6 +1916,8 @@ void copyBufferToImage(
 auto createTextureImage(T)(auto ref T arg) nothrow @nogc @trusted
 if(from!"std.typecons".isTuple!T
     && is(typeof(arg.device) : from!"erupted".VkDevice)
+    && is(typeof(arg.commandPool) : from!"erupted".VkCommandPool)
+    && is(typeof(arg.graphicsQueue) : from!"erupted".VkQueue)
 )
 {
     import util;
